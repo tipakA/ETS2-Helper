a8976cbf2db2dc02f94aaec962090e64f843fe63
tipakA <emka456@onet.pl>
2019-07-08 06:16:12 +0200

    Add function to get powers

--- TDelivery.js
+++ TDelivery.js
@@ -1,6 +1,13 @@
 /* eslint-disable sort-keys, no-inline-comments */
 const Collection = require('collection');
 
+function power(hp) {
+  const powers = {};
+  powers.hp = hp;
+  powers.kw = Math.round(hp * 0.736);
+  return powers;
+}
+
 const engines = new Collection([]);
 
 const transmissions = new Collection([]);
