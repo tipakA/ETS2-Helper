ef9b9db76c948ebf5c2cee2ceaedf3ac736d4ca3
tipakA <emka456@onet.pl>
2019-07-08 06:23:15 +0200

    Add used engines to collection

--- TDelivery.js
+++ TDelivery.js
@@ -8,7 +8,48 @@ function power(hp) {
   return powers;
 }
 
-const engines = new Collection([]);
+const engines = new Collection([
+  [ // D2676 324
+    'D2676 324', {
+      model: 'D2676 324',
+      power: power(440),
+      torque: {
+        nm: 2100,
+        at: [ 1000, 1400 ],
+      },
+    },
+  ],
+  [ // Cursor 13 500
+    'Cursor 13 500', {
+      model: 'Cursor 13 500',
+      power: power(500),
+      torque: {
+        nm: 2300,
+        at: [ 1000, 1500 ],
+      },
+    },
+  ],
+  [ // OM 471 Euro VI
+    'OM 471 Euro VI', {
+      model: 'OM 471 Euro VI',
+      power: power(476),
+      torque: {
+        nm: 2300,
+        at: [1100],
+      },
+    },
+  ],
+  [ // MX-11 320 Euro 6
+    'MX-11 320 Euro 6', {
+      model: 'MX-11 320 Euro 6',
+      power: power(435),
+      torque: {
+        nm: 2100,
+        at: [ 1000, 1450 ],
+      },
+    },
+  ],
+]);
 
 const transmissions = new Collection([]);
 
