0126b10f4a0ccab16b6dbb90e39d4a559699c9af
tipakA <emka456@onet.pl>
2019-07-08 05:57:51 +0200

    Prepare TDelivery.js

--- TDelivery.js
+++ TDelivery.js
@@ -1,1 +1,9 @@
 const Collection = require('collection');
+
+const engines = new Collection([]);
+
+const transmissions = new Collection([]);
+
+const trucks = new Collection([]);
+
+module.exports = trucks;
