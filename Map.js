fbbb6ca27fcae2c20d9b5ee75ecc7a1fcae8dab1
tipakA <emka456@onet.pl>
2019-07-08 06:54:56 +0200

    Guide comment

--- Map.js
+++ Map.js
@@ -1,6 +1,14 @@
 /* eslint-disable sort-keys, no-inline-comments, capitalized-comments */
 const Collection = require('collection');
 
+/*
+ * dealer / laborExchange:
+ *   null: not in the city;
+ *   false: is in the city, but not discovered;
+ *   true: discovered;
+ *   'manufacturer': [dealer only] dealer in the city;
+*/
+
 const map = new Collection([]);
 
 module.exports = map;
