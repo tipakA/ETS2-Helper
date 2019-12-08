78c8b24a43a7f73316e1eb1f0ef2ecee6e691007
tipakA <emka456@onet.pl>
2019-12-08 06:26:09 +0100

    Discover Ostrava

--- Map.js
+++ Map.js
@@ -34,7 +34,7 @@ const map = new Collection([
     'czech', {
       cities: new Collection([
         [ 'brno', { dealer: null, discovered: true, garage: { level: null, owned: false }, laborExchange: false, whole: false }],
-        [ 'ostrava', { dealer: null, discovered: false, garage: { level: null, owned: false }, laborExchange: false, whole: false }],
+        [ 'ostrava', { dealer: null, discovered: true, garage: { level: null, owned: false }, laborExchange: false, whole: false }],
         [ 'praha', { dealer: 'Renault', discovered: true, garage: { level: null, owned: false }, laborExchange: false, whole: false }],
       ]),
     },
