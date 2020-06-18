ea2f9344fa0db869c7e971b777a240447374c0a1
tipakA <emka456@onet.pl>
2020-06-19 00:02:00 +0200

    Discovered Bonifacio

--- src/data/map.ts
+++ src/data/map.ts
@@ -104,7 +104,7 @@ export const map = new Collection<CountryName, Country>([
       cities: new Collection<CityName, City>([
         [ 'ajaccio', { dealer: false, discovered: true, garage: { level: null }, laborExchange: false, whole: false }],
         [ 'bastia', { dealer: null, discovered: false, garage: { level: null }, laborExchange: null, whole: false }],
-        [ 'bonifacio', { dealer: null, discovered: false, garage: { level: null }, laborExchange: null, whole: false }],
+        [ 'bonifacio', { dealer: null, discovered: true, garage: { level: null }, laborExchange: null, whole: false }],
         [ 'bordeaux', { dealer: false, discovered: true, garage: { level: null }, laborExchange: false, whole: false }],
         [ 'bourges', { dealer: false, discovered: true, garage: null, laborExchange: null, whole: false }],
         [ 'brest', { dealer: 'Scania', discovered: true, garage: { level: null }, laborExchange: null, whole: false }],
