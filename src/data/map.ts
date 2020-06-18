9187c89dff36b2673756f069ee18de2e6d87af01
tipakA <emka456@onet.pl>
2020-06-18 23:43:16 +0200

    Discovered L'ille Rousse

--- src/data/map.ts
+++ src/data/map.ts
@@ -117,7 +117,7 @@ export const map = new Collection<CountryName, Country>([
         [ 'la-rochelle', { dealer: null, discovered: false, garage: { level: null }, laborExchange: false, whole: false }],
         [ 'le-havre', { dealer: null, discovered: false, garage: { level: null }, laborExchange: null, whole: false }],
         [ 'le-mans', { dealer: false, discovered: false, garage: { level: null }, laborExchange: null, whole: false }],
-        [ 'lile-rousse', { dealer: null, discovered: false, garage: { level: null }, laborExchange: null, whole: false }],
+        [ 'lile-rousse', { dealer: null, discovered: true, garage: { level: null }, laborExchange: null, whole: false }],
         [ 'lille', { dealer: 'Scania', discovered: true, garage: { level: null }, laborExchange: null, whole: false }],
         [ 'limoges', { dealer: false, discovered: false, garage: { level: null }, laborExchange: null, whole: false }],
         [ 'lyon', { dealer: false, discovered: true, garage: { level: null }, laborExchange: false, whole: false }],
