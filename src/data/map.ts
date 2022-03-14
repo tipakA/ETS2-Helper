1c96e8a1345a9a31c1d5d465fa91b90b1364d639
tipakA <emka456@onet.pl>
2022-03-14 22:49:02 +0100

    Discovered Algeciras

--- src/data/map.ts
+++ src/data/map.ts
@@ -343,7 +343,7 @@ export const map = new Collection<CountryName, Country>([
       cities: new Collection<CityName, City>([
         [ 'a coruna', { dealer: null, discovered: false, garage: { level: null }, laborExchange: null, whole: false }],
         [ 'albacete', { dealer: false, discovered: true, garage: { level: null }, laborExchange: null, whole: false }],
-        [ 'algeciras', { dealer: null, discovered: false, garage: { level: null }, laborExchange: null, whole: false }],
+        [ 'algeciras', { dealer: null, discovered: true, garage: { level: null }, laborExchange: null, whole: false }],
         [ 'almaraz', { dealer: null, discovered: false, garage: null, laborExchange: null, whole: false }],
         [ 'almeria', { dealer: 'Iveco', discovered: true, garage: { level: null }, laborExchange: null, whole: false }],
         [ 'badajoz', { dealer: false, discovered: false, garage: null, laborExchange: null, whole: false }],
